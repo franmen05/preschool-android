f9bfdda14b081a644fadbf107b8216918ad51f95
franmen05 <franmen05@gmail.com>
2022-06-12 12:25:04 -0400

    first commit

---
+++ app/src/main/java/com/preschool/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.preschool.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
